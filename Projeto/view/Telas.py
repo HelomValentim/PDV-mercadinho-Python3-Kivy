09c2948fa2c7ba9c603a83687279d3282c92f5f9
waltersilva5 <waltersilva5@outlook.com>
2018-08-31 10:17:57 -0300

    corrigido erro nas telas

--- Projeto/view/Telas.py
+++ Projeto/view/Telas.py
@@ -1,4 +1,6 @@
 from kivy.app import App
+from kivy.lang import Builder
+from sys import path
 from Projeto.model.usuario import Usuario
 from kivy.uix.label import Label
 from kivy.uix.button import Button
@@ -7,29 +9,30 @@ from kivy.uix.textinput import TextInput
 from kivy.uix.screenmanager import ScreenManager, Screen
 
 class GerenciadorDeTelas(ScreenManager):
-    pass
+    def clicar(self):
+        print("clicado")
+        self.current = "Tela1"
 
 class Tela1(Screen):
     pass
+
 
 class Tela2(Screen):
     pass
 
 class TelaDeLogin(Screen):
-    def __init__(self):
-        super(TelaDeLogin, self)
-        self.ids.botaoEntrar.on_press = self.entrarNoSistema()
-
-    def entrarNoSistema(self):
-        print("entrou")
-
+    pass
 
 class TelaInicial(App):
     def build(self):
         self.title = "Ponto De Vendas"
-        self.telaLogin = TelaDeLogin()
+        self.telaDeLogin = TelaDeLogin()
+
         self.tela1 = Tela1()
         self.tela2 = Tela2()
         self.gerenciador = GerenciadorDeTelas()
+
+        #self.telaPrincipal = Builder.load_file(path[1]+"/Projeto/view/telainicial.kv")
         return self.gerenciador
+
 
