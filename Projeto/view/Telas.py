dee95e823aba7d6bf8d9c952e645231cb633d8ca
waltersilva5 <waltersilva5@outlook.com>
2018-08-31 13:34:16 -0300

    adicionado opção de esconder senha

--- Projeto/view/Telas.py
+++ Projeto/view/Telas.py
@@ -1,32 +1,33 @@
+from Projeto.model.usuario import Usuario
+
 from kivy.app import App
 from kivy.lang import Builder
-from sys import path
-from Projeto.model.usuario import Usuario
 from kivy.uix.label import Label
 from kivy.uix.button import Button
 from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.textinput import TextInput
 from kivy.uix.screenmanager import ScreenManager, Screen
 
+from kivy.core.window import Window
+Window.size = (1024, 720)
+
 class GerenciadorDeTelas(ScreenManager):
     pass
 
-class Tela1(Screen):
+class TelaSistema(Screen):
     pass
-
 
 class TelaDeLogin(Screen):
     pass
-
 
 class TelaInicial(App):
     def build(self):
         self.title = "Ponto De Vendas"
         self.gerenciador = GerenciadorDeTelas()
         self.telaDeLogin = TelaDeLogin()
-        self.tela1 = Tela1()
+        self.TelaSistema = TelaSistema()
 
-        self.gerenciador.add_widget(self.tela1)
+        self.gerenciador.add_widget(self.TelaSistema)
         self.gerenciador.add_widget(self.telaDeLogin)
 
         return self.gerenciador
@@ -34,7 +35,8 @@ class TelaInicial(App):
     def entrarNoSistema(self, login, senha):
         self.usuario = Usuario()
         if (self.usuario.testaLoginSenha(login, senha)):
-            self.gerenciador.current="Tela1"
+            self.gerenciador.current = "TelaSistema"
         else:
             self.root.get_screen('TelaDeLogin').ids.labelMensagemLogin.text = "Login ou senha invalidos"
+
 
